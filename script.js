844fb5242192c595b972fe99934482c80829dff1
daredevil221509-hue <daredevil221509@gmail.com>
2026-07-07 12:16:28 +0530

    Version 33 - Premium Effects Pack

--- script.js
+++ script.js
@@ -346,3 +346,9 @@ faqItems.forEach(item => {
     });
 
 });
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove",(e)=>{
+    cursor.style.left = e.clientX + "px";
+    cursor.style.top = e.clientY + "px";
+});
