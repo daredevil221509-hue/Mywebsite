108c1ffac84616a7cd6370c3e3994a3babc164bc
daredevil221509-hue <daredevil221509@gmail.com>
2026-07-07 11:26:13 +0530

    Version 25 - FAQ Accordion

--- script.js
+++ script.js
@@ -331,3 +331,18 @@ if (newsletter) {
     });
 
 }
+// FAQ Accordion
+
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+
+    const question = item.querySelector(".faq-question");
+
+    question.addEventListener("click", () => {
+
+        item.classList.toggle("active");
+
+    });
+
+});
